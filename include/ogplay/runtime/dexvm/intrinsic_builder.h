c0607c3cbe828aff60e4b4017abe885a91e14d1c
poping520 <poping52096@gmail.com>
2026-08-19 21:07:20 +0800

    format code

--- include/ogplay/runtime/dexvm/intrinsic_builder.h
+++ include/ogplay/runtime/dexvm/intrinsic_builder.h
@@ -6,41 +6,39 @@
 #include "ogplay/runtime/dexvm/class_linker.h"
 
 namespace ogplay::runtime::dexvm {
+    class IntrinsicClassBuilder final {
+    public:
+        explicit IntrinsicClassBuilder(std::string descriptor);
 
-class IntrinsicClassBuilder final {
-public:
-    explicit IntrinsicClassBuilder(std::string descriptor);
+        IntrinsicClassBuilder& Super(std::string descriptor);
 
-    IntrinsicClassBuilder& Super(std::string descriptor);
-    IntrinsicClassBuilder& Implements(std::string descriptor);
-    IntrinsicClassBuilder& MarkInterface();
-    IntrinsicClassBuilder& Static(std::string name, std::string descriptor,
-                                  IntrinsicHandler handler);
-    IntrinsicClassBuilder& Virtual(std::string name, std::string descriptor,
-                                   IntrinsicHandler handler);
-    IntrinsicClassBuilder& Overridable(std::string name,
-                                       std::string descriptor,
-                                       IntrinsicHandler handler);
-    IntrinsicClassBuilder& Unimplemented(std::string name,
-                                         std::string descriptor,
-                                         bool is_static, bool overridable);
-    IntrinsicClassBuilder& Field(std::string name, std::string descriptor,
-                                 bool is_static);
-    IntrinsicClassBuilder& ConstantInt(std::string name,
-                                       std::string descriptor,
-                                       std::int64_t value);
-    IntrinsicClassBuilder& ConstantString(std::string name,
-                                          std::string value);
-    IntrinsicClassBuilder& Clinit(IntrinsicHandler handler);
+        IntrinsicClassBuilder& Implements(std::string descriptor);
 
-    [[nodiscard]] IntrinsicClassDecl Build() &&;
+        IntrinsicClassBuilder& MarkInterface();
 
-private:
-    IntrinsicClassBuilder& Method(std::string name, std::string descriptor,
-                                  bool is_static, bool overridable,
-                                  IntrinsicHandler handler);
+        IntrinsicClassBuilder& Static(std::string name, std::string descriptor, IntrinsicHandler handler);
 
-    IntrinsicClassDecl declaration_;
-};
+        IntrinsicClassBuilder& Virtual(std::string name, std::string descriptor, IntrinsicHandler handler);
 
-}  // namespace ogplay::runtime::dexvm
+        IntrinsicClassBuilder& Overridable(std::string name, std::string descriptor, IntrinsicHandler handler);
+
+        IntrinsicClassBuilder& Unimplemented(std::string name, std::string descriptor, bool is_static,
+                                             bool overridable);
+
+        IntrinsicClassBuilder& Field(std::string name, std::string descriptor, bool is_static);
+
+        IntrinsicClassBuilder& ConstantInt(std::string name, std::string descriptor, std::int64_t value);
+
+        IntrinsicClassBuilder& ConstantString(std::string name, std::string value);
+
+        IntrinsicClassBuilder& Clinit(IntrinsicHandler handler);
+
+        [[nodiscard]] IntrinsicClassDecl Build() &&;
+
+    private:
+        IntrinsicClassBuilder& Method(std::string name, std::string descriptor, bool is_static, bool overridable,
+                                      IntrinsicHandler handler);
+
+        IntrinsicClassDecl declaration_;
+    };
+} // namespace ogplay::runtime::dexvm
